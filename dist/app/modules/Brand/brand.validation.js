1c441203b23d8c08ee45e8880c79fb2d15b3b276
sopnilali <sopnilali24@gmail.com>
2025-05-15 21:40:12 +0600

    brand validation update

--- dist/app/modules/Brand/brand.validation.js
+++ dist/app/modules/Brand/brand.validation.js
@@ -5,13 +5,12 @@ const zod_1 = require("zod");
 const createBrandValidation = zod_1.z.object({
     name: zod_1.z.string({ required_error: "Name is required" }),
     description: zod_1.z.string({ required_error: "Description is required" }),
-    image: zod_1.z.string({ required_error: "Image is required" }).optional(),
+    logo: zod_1.z.string({ required_error: "Logo is required" }).optional(),
 });
 const updateBrandValidation = zod_1.z.object({
     name: zod_1.z.string({ required_error: "Name is required" }).optional(),
     description: zod_1.z.string({ required_error: "Description is required" }).optional(),
-    image: zod_1.z.string({ required_error: "Image is required" }).optional(),
-    status: zod_1.z.string({ required_error: "Status is required" }).optional(),
+    logo: zod_1.z.string({ required_error: "Image is required" }).optional(),
 });
 exports.BrandValidation = {
     createBrandValidation,
--- src/app/modules/Brand/brand.validation.ts
+++ src/app/modules/Brand/brand.validation.ts
@@ -4,14 +4,13 @@ import { z } from "zod";
 const createBrandValidation = z.object({
     name: z.string({required_error: "Name is required"}),
     description: z.string({required_error: "Description is required"}),
-    image: z.string({required_error: "Image is required"}).optional(),
+    logo: z.string({required_error: "Logo is required"}).optional(),
 });
 
 const updateBrandValidation = z.object({
     name: z.string({required_error: "Name is required"}).optional(),
     description: z.string({required_error: "Description is required"}).optional(),
-    image: z.string({required_error: "Image is required"}).optional(),
-    status: z.string({required_error: "Status is required"}).optional(),
+    logo: z.string({required_error: "Image is required"}).optional(),
 });
 
 export const BrandValidation = {
